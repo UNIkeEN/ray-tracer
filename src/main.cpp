ac185cb217b4d1ac074895f38b736679b1800c1d
UNIkeEN <2641712741@qq.com>
2023-11-24 15:12:47 +0000

    chore: add cornell box model

--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,7 @@
 #include "utils.h"
 #include "material.h"
 #include "sphere.h"
+#include "triangle.h"
 
 void write_image(const int& height, const int& width, std::vector<Vector3>& buffer) {
     
@@ -31,29 +32,43 @@ void write_image(const int& height, const int& width, std::vector<Vector3>& buff
 int main(){
 
     // Configs of image
-    int image_height = 900;
-    int image_width = 1200;
+    int image_height = 784;
+    int image_width = 784;
 
     // Configs of renderer
-    int max_ray_bounces = 10;
-    int spp = 100;
+    int max_ray_bounces = 50;
+    int spp = 1000;
 
     // Configs of camera
     int vfov = 40;
-    Vector3 lookfrom = Vector3(0, 0, -1);
-    Vector3 lookat = Vector3(0, 0, 0);
+    Vector3 lookfrom = Vector3(278, 278, -300);
+    Vector3 lookat = Vector3(278, 278, 0);
     Vector3 background_color = Vector3(0 ,0, 0);
     
     // Create Scene
     Scene scene;
 
-    auto* mat1 = new Lambertian(Vector3(0.8, 0.1, 0.1));
+    //cornell box
+    auto* mat_red = new Lambertian(Vector3(0.65f, 0.05f, 0.05f));
+    auto* mat_green = new Lambertian(Vector3(0.12f, 0.45f, 0.15f));
+    auto* mat_white = new Lambertian(Vector3(0.73f, 0.73f, 0.73f));
 
-    auto* s1 = new Sphere(Vector3(0, 5, 20), 4.0, new BasicLight());
-    auto* s2 = new Sphere(Vector3(0, -5, 20), 4.0, mat1);
+    MeshTriangle floor("../models/cornellbox/floor.obj", mat_white);
+    MeshTriangle shortbox("../models/cornellbox/shortbox.obj", mat_white) ;
+    MeshTriangle tallbox("../models/cornellbox/tallbox.obj", mat_white);
+    MeshTriangle left("../models/cornellbox/left.obj", mat_red);
+    MeshTriangle right("../models/cornellbox/right.obj", mat_green);
+    MeshTriangle toplight("../models/cornellbox/light.obj", new BasicLight());
 
-    scene.add(s1);
-    scene.add(s2);
+    Sphere test(Vector3(278, 278, 200), 100, mat_red);
+
+    scene.add(&floor);
+    scene.add(&shortbox);
+    scene.add(&tallbox);
+    scene.add(&left);
+    scene.add(&right);
+    scene.add(&toplight);
+//    scene.add(&test);
 
     // Create Camera
     Camera camera(image_height, image_width);
